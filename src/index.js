07d5e32ad513f4af4947c72683cac9237d3dafd4
Alexandra Vlasova <Alex.Genrietta@gmail.com>
2021-08-15 18:21:22 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    let reverse = 0
+
+  while (n > 0) {
+      reverse = reverse * 10 + n % 10
+      n = parseInt(n / 10)
+  }
+
+  return reverse
 }
